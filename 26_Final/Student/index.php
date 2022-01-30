c42e1b4934df7d0eb367138096ef58aeee6609e5
hasanosman2020 <hasan79.hoa@gmail.com>
2022-01-30 05:49:14 +0000

    Docs - complete lesson 26 and build the basic template of a web page

---
+++ 26_Final/Student/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Coding Dynamic Websites</title>
+        <link rel='stylesheet' href='assets/styles.css'>
+    </head>
+
+    <body id='final_example'>
+        <div class='wrapper'>
+            <div id='banner'>
+                <a href=" " title='Return to Home'>
+                    <img src='img/banner.php' alt='Fine Dining';
+                </a>
+
+            </div>
+            <div id='nav'>
+                <ul>
+                    <li><a href='index.php'>Home</a></li>
+                    <li><a href='team.php'>Team</a></li>
+                    <li><a href='menu.php'>Menu</a></li>
+                    <li><a href='contact.php'>Contact</a></li>
+                </ul>
+
+            </div>
+            <div class='content'>
+                <div id='footer' class='cf'>
+
+                <div class='column three'>
+                    <strong>Phone</strong>+44 737 6095 247
+                </div>
+                <div class='column three'>
+                    <strong>Location</strong>Borough of Islington, London, United Kingdom
+                </div>
+                <div class='column three last'>
+                    <strong>Hours</strong>
+                    <em>Monday - Friday</em><br>
+                    1000hrs - 1900hrs<br><br>
+                    <em>Saturday - Sunday</em><br>1200hrs - 1700hrs<br><br>
+
+
+            </div>
+                </div>
+                <small>&copy;2022 Hasan Osman</small>
+                </div>
+            </div>
+            <div class='copyright-info'>
+            
+            </div>
+        </div>
+
+    </body>
+</html>
