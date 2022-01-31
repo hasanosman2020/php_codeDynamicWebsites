359bbd6c9b1119937d24820342327df484be0df2
hasanosman2020 <hasan79.hoa@gmail.com>
2022-01-31 13:20:45 +0000

    Docs: create global header and global footer and dynmic nav

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,27 @@
+<div id='footer' class='cf'>
+
+                <div class='column three'>
+                    <strong>Phone</strong> +44 737 6095 247
+                </div><br>
+                <div class='column three'>
+                    <strong>Location</strong> Borough of Islington, London, United Kingdom
+                </div><br>
+                <div class='column three last'>
+                    <strong>Hours</strong>
+                    <em> Monday - Friday </em><br>
+                    1000hrs - 1900hrs<br>
+                    <em>Saturday - Sunday</em><br>1200hrs - 1700hrs<br><br>
+
+
+            </div>
+                </div>
+                <small>&copy;2022 Hasan Osman</small>
+                </div>
+            </div>
+            <div class='copyright-info'>
+            
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ 26_Final/Student/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+include('includes/arrays.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Coding Dynamic Websites</title>
+        <link rel='stylesheet' href='../assets/styles.css'>
+    </head>
+
+    <body id='final_example'>
+        <div class='wrapper'>
+            <div id='banner'>
+                <a href="# " title='Return to Home'>
+                    <img src='./img/banner.png' alt='Fine Dining';
+                </a>
+
+            </div>
+            <div id='nav'>
+                    <?php include('includes/nav.php'); ?>
+            </div>
+            <div class='content'>
